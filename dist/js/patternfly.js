1a75d53d83c8c33c8c3e4f4fb3336cc9f04dfadf
Robb Hamilton <rhamilto@redhat.com>
2014-02-19 10:17:40 -0500

    Updating to Bootstrap v3.1.1; beginning of basic page layouts

--- dist/js/patternfly.js
+++ dist/js/patternfly.js
@@ -1,6 +1,32 @@
 // PatternFly Namespace
 var PatternFly = PatternFly || {};
 
+$(document).ready(function() {
+  // Call sidebar() on ready if .sidebar-pf exists
+  if ($('.sidebar-pf').length > 0) {
+    sidebar();
+  }
+});
+
+$(window).resize(function() {
+  // Call sidebar() on resize if .sidebar-pf exists
+  if ($('.sidebar-pf').length > 0) {
+    sidebar();
+  }
+});
+
+// PatternFly Sidebar 
+// Set height of sidebar-pf to height of viewport minus height of navbar-pf if not mobile
+function sidebar() {
+  var viewportHeight = $(window).height();
+  var navbarpfHeight = $('.navbar-pf').outerHeight();
+  var colHeight = 0;
+  if ( $('.navbar-pf .navbar-toggle').is(':hidden')) {
+    colHeight = viewportHeight - navbarpfHeight;
+  }
+  $('.sidebar-pf').parent('.row').children('[class*="col-"]').css({ "min-height":colHeight});
+}
+
 // Util: PatternFly Popovers
 // Add data-close="true" to insert close X icon
 (function($) {
